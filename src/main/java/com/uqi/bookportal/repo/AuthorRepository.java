a5cc0d247e30249070f41a6ecbd82ef0e7a38787
Uğurcan Erdoğan <ugurcanerdogan3306@gmail.com>
2022-08-04 15:06:36 +0300

    repositories modified

--- src/main/java/com/uqi/bookportal/repo/AuthorRepository.java
+++ src/main/java/com/uqi/bookportal/repo/AuthorRepository.java
@@ -1,9 +1,15 @@
 package com.uqi.bookportal.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.uqi.bookportal.model.Author;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+	Optional<Author> findByName(String name);
+
+	boolean existsByName(String name);
 
 }
--- src/main/java/com/uqi/bookportal/repo/BookRepository.java
+++ src/main/java/com/uqi/bookportal/repo/BookRepository.java
@@ -1,9 +1,15 @@
 package com.uqi.bookportal.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.uqi.bookportal.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+	Optional<Book> findByTitle(String title);
+
+	boolean existsByTitle(String title);
 
 }
