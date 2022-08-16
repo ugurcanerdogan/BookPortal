d6ea7613ff606bc1da7e1affe866b08fae25ceb2
Uğurcan Erdoğan <ugurcanerdogan3306@gmail.com>
2022-08-17 00:31:33 +0300

    services added

---
+++ book-portal-fe/src/services/UserService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export default class UserService {
+
+  getUserByUsername = (username) => {
+    return axios.get(`http://localhost:8080/api/v1/users/by-username?username=${username}`);
+  };
+
+  getUsers = () => {
+    return axios.get("http://localhost:8080/api/v1/users/");
+  };
+
+  getUsersWithPagination = (username, pageNumber = 0, pageSize = 4) => {
+    return axios.get(`http://localhost:8080/api/v1/users/with-jpa-pagination?pageNumber=${pageNumber}&pageSize=${pageSize}&currentUser=${username}`);
+  };
+
+  updateUser = (userId, body) => {
+    return axios.put(`http://localhost:8080/api/v1/users/${userId}`, body);
+  };
+
+  addUser = (body) => {
+    return axios.post(`http://localhost:8080/api/v1/users`, body);
+  };
+}
