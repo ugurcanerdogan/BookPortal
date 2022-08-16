55093b1e05d65b077db9c863223a3b5410ab6d79
Uğurcan Erdoğan <ugurcanerdogan3306@gmail.com>
2022-08-17 00:31:52 +0300

    utilities-customs added

---
+++ book-portal-fe/src/utilities/apiProgress.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const useApiProgress = (apiMethod, apiPath) => {
+  const [pendingApiCall, setPendingApiCall] = useState(false);
+  useEffect(() => {
+
+    let requestInterceptor, responseInterceptor;
+
+    const updateApiCallFor = (method, url, inProgress) => {
+      if (url.startsWith(apiPath) && method === apiMethod) {
+        setPendingApiCall(inProgress);
+      }
+    };
+
+    const regiserInterceptors = () => {
+      requestInterceptor = axios.interceptors.request.use((request) => {
+        const { url, method } = request;
+        updateApiCallFor(method, url, true);
+        return request;
+      });
+      responseInterceptor = axios.interceptors.response.use((response) => {
+        const { url, method } = response.config;
+        updateApiCallFor(method, url, false);
+        return response;
+      }, (error) => {
+        const { url, method } = error.config;
+        updateApiCallFor(method, url, false);
+        throw error;
+      });
+    };
+
+    const unregisterInterceptors = () => {
+      axios.interceptors.request.eject(requestInterceptor);
+      axios.interceptors.response.eject(responseInterceptor);
+    };
+
+    regiserInterceptors();
+
+    return function unmount() {
+      unregisterInterceptors();
+    };
+  }, [apiPath, apiMethod]);
+  return pendingApiCall;
+};
