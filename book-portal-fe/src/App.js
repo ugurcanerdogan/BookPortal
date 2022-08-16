74c8305768a30bb4dda9d0a4b735a93e3c68abe7
Uğurcan Erdoğan <ugurcanerdogan3306@gmail.com>
2022-08-17 00:32:18 +0300

    app and assets added

---
+++ book-portal-fe/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import { Container } from "semantic-ui-react";
+import Dashboard from "./layouts/Dashboard";
+import Navi from "./layouts/Navi";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navi />
+      <Container className="main">
+        <Dashboard />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
