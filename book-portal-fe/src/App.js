d558d7731b0394888d5d999643537712f46c94de
Uğurcan Erdoğan <ugurcanerdogan3306@gmail.com>
2022-08-18 15:00:36 +0300

    toastify integrated

--- book-portal-fe/src/App.js
+++ book-portal-fe/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Container } from "semantic-ui-react";
+import { ToastContainer } from "react-toastify";
 import Dashboard from "./layouts/Dashboard";
 import Navi from "./layouts/Navi";
 
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <div className="App">
+      <ToastContainer theme="colored" position="bottom-right"/>
       <Navi />
       <Container className="main">
         <Dashboard />
