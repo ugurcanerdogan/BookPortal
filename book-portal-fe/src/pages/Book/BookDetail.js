90d5cd12c0f6b5b8ea2fc07aaf773bc07be19c37
Uğurcan Erdoğan <ugurcanerdogan3306@gmail.com>
2022-08-21 23:20:09 +0300

    add/remove lists integrated

--- book-portal-fe/src/pages/Book/BookDetail.js
+++ book-portal-fe/src/pages/Book/BookDetail.js
@@ -1,25 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Button, Card, Icon, Image } from "semantic-ui-react";
+import { Button, Card, Dimmer, Icon, Image, Loader, Message, Segment } from "semantic-ui-react";
+import UserService from "../../services/UserService";
 import BookService from "../../services/BookService";
 import bookPicture from "../../assets/bookImage.png";
 import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 import { toast } from "react-toastify";
+import { useApiProgress } from "../../utilities/apiProgress";
 
 const BookDetail = (props) => {
 
   const { t } = useTranslation();
-  const { isAdmin } = useSelector(state => state.auth);
+  const { id, isAdmin } = useSelector(state => state.auth);
   const [book, setBook] = useState({});
   const [editable, setEditable] = useState(false);
+  const [loadFailure, setLoadFailure] = useState(false);
+  const userService = new UserService();
   const bookService = new BookService();
   const { history } = props;
   const { push } = history;
   let { isbn } = useParams();
+  const pendingApiCall = useApiProgress("get", "http://localhost:8080/api/v1/books/by-isbn?isbn=");
 
   useEffect(() => {
-    bookService.getBookByIsbn(isbn).then(book => setBook(book.data));
+    setLoadFailure(false);
+    bookService.getBookByIsbn(isbn).then(book => setBook(book.data)).catch(error => setLoadFailure(true));
   }, []);
 
   useEffect(() => {
@@ -32,7 +38,45 @@ const BookDetail = (props) => {
     push("/books")
   }
 
+  const addToReadList = async () => {
+    try{
+      await userService.addBookToReadList(id,book.id)
+      toast.success("Book is added to your reading list!")
+    }catch (e){
+      toast.error("Book is already in your reading list!")
+    }
+  };
+
+  const addToFavList = async () => {
+    try{
+      await userService.addBookToFavList(id,book.id)
+      toast.success("Book is added to your favorite list!")
+    }catch (e){
+      toast.error("Book is already in your favorite list!")
+    }
+  };
+
+  let loadFail = (
+    <Message negative>
+      <Message.Header>{t("We couldn't load the book...")}</Message.Header>
+      <p>{t("Error occurred...")}</p>
+    </Message>
+  );
+
+  if (pendingApiCall) {
+    return (
+      (    <Segment>
+          <Dimmer active inverted>
+            <Loader inverted content={t("Loading")} />
+          </Dimmer>
+          <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+        </Segment>
+      )
+    )
+  }
+
   return (
+    !loadFailure ?
     <div>
     <Card.Group>
       <Card fluid>
@@ -48,10 +92,10 @@ const BookDetail = (props) => {
         </Card.Content>
         <Card.Content extra>
           <div className="ui two buttons">
-            <Button basic color="green">
+            <Button onClick={() => addToReadList()} basic color="green">
               <Icon name="book" /> {t("Add to Read List")}
             </Button>
-            <Button basic color="red">
+            <Button onClick={() => addToFavList()} basic color="red">
               <Icon name="heart" /> {t("Add to Fav List")}
             </Button>
           </div>
@@ -72,7 +116,9 @@ const BookDetail = (props) => {
         }
       </Card>
     </Card.Group>
-  </div>);
+  </div>
+      : loadFail
+  );
 };
 
 export default BookDetail;
